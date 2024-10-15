4af82f8dae6cf45eb8bb24fc4be6ece4fc0235d4
Follow the Four Leafed Clover 🍀 <55417207+followthefourleafedclover@users.noreply.github.com>
2024-10-15 11:53:53 -0400

    Create citations.py

---
+++ scripts/citations.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.header("Acknowledgements")
+st.divider() 
+
+st.write("""Thank you to the following for making this project possible:""")
+st.write('- The Allen Institute for Brain Science, for providing the dataset used in this analysis')
+
+st.markdown("""### Citations""") 
+st.write(""" - Nahm FS. Nonparametric statistical tests for the continuous data: the basic concept and the practical use. Korean J Anesthesiol. 2016 Feb;69(1):8-14. doi: 10.4097/kjae.2016.69.1.8. Epub 2016 Jan 28. PMID: 26885295; PMCID: PMC4754273.\n
+""")
+st.write("""- Zizhen Yao, Cindy T.J. van Velthoven, Thuc Nghi Nguyen, Jeff Goldy, Adriana E. Sedeno-Cortes, Fahimeh Baftizadeh, Darren Bertagnolli, Tamara Casper, Megan Chiang, Kirsten Crichton, Song-Lin Ding, Olivia Fong, Emma Garren, Alexandra Glandon, Nathan W. Gouwens, James Gray, Lucas T. Graybuck, Michael J. Hawrylycz, Daniel Hirschstein, Matthew Kroll, Kanan Lathia, Changkyu Lee, Boaz Levi, Delissa McMillen, Stephanie Mok, Thanh Pham, Qingzhong Ren, Christine Rimorin, Nadiya Shapovalova, Josef Sulc, Susan M. Sunkin, Michael Tieu, Amy Torkelson, Herman Tung, Katelyn Ward, Nick Dee, Kimberly A. Smith, Bosiljka Tasic, Hongkui Zeng,
+            A taxonomy of transcriptomic cell types across the isocortex and hippocampal formation,
+            Cell,
+            Volume 184, Issue 12,
+            2021,
+            Pages 3222-3241.e26,
+            ISSN 0092-8674,
+            https://doi.org/10.1016/j.cell.2021.04.021.
+            (https://www.sciencedirect.com/science/article/pii/S0092867421005018)
+            \nAbstract: Summary
+            The isocortex and hippocampal formation (HPF) in the mammalian brain play critical roles in perception, cognition, emotion, and learning. We profiled ∼1.3 million cells covering the entire adult mouse isocortex and HPF and derived a transcriptomic cell-type taxonomy revealing a comprehensive repertoire of glutamatergic and GABAergic neuron types. Contrary to the traditional view of HPF as having a simpler cellular organization, we discover a complete set of glutamatergic types in HPF homologous to all major subclasses found in the six-layered isocortex, suggesting that HPF and the isocortex share a common circuit organization. We also identify large-scale continuous and graded variations of cell types along isocortical depth, across the isocortical sheet, and in multiple dimensions in hippocampus and subiculum. Overall, our study establishes a molecular architecture of the mammalian isocortex and hippocampal formation and begins to shed light on its underlying relationship with the development, evolution, connectivity, and function of these two brain structures.
+            Keywords: single-cell RNA sequencing; single-cell transcriptomics; cell type; cortex; hippocampus; glutamatergic; GABAergic; excitatory neuron; interneuron
+""")
